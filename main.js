ea7a4a93627a934ec4d1f209c861b6a189b5826e
na-ma-na723 <namanbhandari723@gmail.com>
2022-10-17 23:06:55 +0530

    added code part & buttons with full functionality

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const outputText = document.getElementById("outputText");
 const key = document.getElementById("key");
 
 
+
+const cppBtn = document.getElementById("cppButton");
+const pyBtn = document.getElementById("pyButton");
+const jsBtn = document.getElementById("jsButton");
+
+// const languageBtn = document.getElementsByClassName("langbtn")
+
+const cppCode = document.getElementById("cpp");
+const pyCode = document.getElementById("python");
+const jsCode = document.getElementById("javascript");
+
 function vigenereKeyLength(key, msg) {
     let keyLength = key.length;
     for(let i=0;i < msg.length - keyLength ; i++) {
@@ -68,5 +79,111 @@ function decryptButtonCall() {
     inputText.value =  vigenereDecryptCall(outputText.value, key.value);
 }
 
+
 document.querySelector("#encryptButton").addEventListener("click",encryptButtonCall)
 document.querySelector("#decryptButton").addEventListener("click",decryptButtonCall)
+
+
+cppBtn.addEventListener("click", ()=>{
+
+    if(cppBtn.value === "notselected"){
+
+        cppBtn.value = "selected";
+        pyBtn.value = "notselected";
+        jsBtn.value = "notselected";
+
+        cppBtn.style.backgroundColor = "#272822";
+        pyBtn.style.backgroundColor = "#131417";
+        jsBtn.style.backgroundColor = "#131417";
+
+        cppBtn.style.border = "2px solid white"
+        pyBtn.style.border = "0";
+        jsBtn.style.border = "0";
+
+        cppBtn.style.borderBottom = "0";
+        pyBtn.style.borderBottom = "2px solid white";
+        jsBtn.style.borderBottom = "2px solid white";
+
+        cppBtn.style.color = "white";
+        pyBtn.style.color = "yellow";
+        jsBtn.style.color = "yellow";
+        
+        cppCode.style.opacity = "1" ;
+        pyCode.style.opacity = "0" ;
+        jsCode.style.opacity = "0" ;
+    }
+
+})
+
+pyBtn.addEventListener("click", ()=>{
+
+    if(pyBtn.value === "notselected"){
+
+        cppBtn.value = "notselected";
+        pyBtn.value = "selected";
+        jsBtn.value = "notselected";
+
+        cppBtn.style.backgroundColor = "#131417";
+        pyBtn.style.backgroundColor = "#272822";
+        jsBtn.style.backgroundColor = "#131417";
+
+        cppBtn.style.border = "0"
+        pyBtn.style.border = "2px solid white";
+        jsBtn.style.border = "0";
+
+        cppBtn.style.borderBottom = "2px solid white";
+        pyBtn.style.borderBottom = "0";
+        jsBtn.style.borderBottom = "2px solid white";
+
+        cppBtn.style.color = "yellow";
+        pyBtn.style.color = "white";
+        jsBtn.style.color = "yellow";
+        
+
+        cppCode.style.opacity = "0" ;
+        pyCode.style.opacity = "1" ;
+        jsCode.style.opacity = "0" ;
+    }
+
+})
+
+jsBtn.addEventListener("click", ()=>{
+
+    if(jsBtn.value === "notselected"){
+
+        cppBtn.value = "notselected";
+        pyBtn.value = "notselected";
+        jsBtn.value = "selected";
+
+        cppBtn.style.backgroundColor = "#131417";
+        pyBtn.style.backgroundColor = "#131417";
+        jsBtn.style.backgroundColor = "#272822";
+
+        cppBtn.style.border = "0"
+        pyBtn.style.border = "0";
+        jsBtn.style.border = "2px solid white";
+
+        cppBtn.style.borderBottom = "2px solid white";
+        pyBtn.style.borderBottom = "2px solid white";
+        jsBtn.style.borderBottom = "0";
+
+        cppBtn.style.color = "yellow";
+        pyBtn.style.color = "yellow";
+        jsBtn.style.color = "white";
+
+        cppCode.style.opacity = "0" ;
+        pyCode.style.opacity = "0" ;
+        jsCode.style.opacity = "1" ;
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
